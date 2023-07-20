5a6f8753619ebad75d06593b359d19dd4d591e01
shahariyr reza <shahariyr.reza@hotmail.com>
2023-07-20 23:55:31 +0600

    update main

--- src/main/java/com/example/main/DepInjectionApplication.java
+++ src/main/java/com/example/main/DepInjectionApplication.java
@@ -3,16 +3,18 @@ package com.example.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 import com.example.model.coder;
 
 @SpringBootApplication
+@ImportResource ({"classpath:beanConfig.xml"})
 public class DepInjectionApplication {
 
 	public static void main(String[] args) {
 	 	ApplicationContext context = SpringApplication.run(DepInjectionApplication.class, args);
 		
-		coder c1 = (coder) context.getBean("");
+		coder c1 = (coder) context.getBean("coder1");
 	}
 
 }
